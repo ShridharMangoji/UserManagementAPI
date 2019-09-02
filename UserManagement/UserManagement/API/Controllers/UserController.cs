2b08523666eacf5377be6b25a40f5455aa868dfd
shridhar mangoji <shridhar.mangoji@gmail.com>
2019-09-03 00:54:02 +0530

    Final Commit

---
+++ UserManagement/UserManagement/API/Controllers/UserController.cs
@@ -0,0 +1,216 @@
+﻿using API.Models;
+using API.Util;
+using BAL.DbOperation;
+using DAL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("User")]
+    public class UserController : ApiController
+    {
+        [NonAction]
+        public bool IsUserValid(string email, string password, out string statusMessage)
+        {
+            bool isValid = false;
+            statusMessage = "User is either locked or Inactive to perform any action";
+            UserCRUD userCRUD = new UserCRUD();
+            var user = userCRUD.GetUser(email, password);
+            if (user!=null)
+            {
+                if (user.is_active==true)
+                {
+                    if ((user.is_locked!=true))
+                    {
+                        statusMessage = string.Empty;
+                        isValid = true;
+                    }
+                }
+            }
+            else
+            {
+                statusMessage = HttpStatusCode.Unauthorized.ToString();
+            }
+
+            return isValid;
+        }
+
+        [Route("Authenticate")]
+        public AuthenticeResponse Authenticate(AuthenticateCR req)
+        {
+            AuthenticeResponse resp = new AuthenticeResponse();
+            try
+            {
+                if (ValidateRequest.Authenticate(req))
+                {
+                    UserCRUD userCRUD = new UserCRUD();
+                    string statusMessage = string.Empty;
+                    if (IsUserValid(req.EmailID, req.Password, out statusMessage))
+                    {
+                        resp.Token = JWTService.GenerateToken(req.EmailID);
+                        resp.StatusCode = HttpStatusCode.OK;
+                        resp.StatusMessage = HttpStatusCode.OK.ToString();
+                    }
+                    else
+                    {
+                        resp.StatusCode = HttpStatusCode.Unauthorized;
+                        resp.StatusMessage = statusMessage;
+                    }
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.StatusMessage = HttpStatusCode.BadRequest.ToString();
+                }
+            }
+            catch (Exception es)
+            {
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                resp.StatusMessage = HttpStatusCode.InternalServerError.ToString();
+            }
+            return resp;
+        }
+
+        [Route("AddUser")]
+        public BaseResponse AddUser(UserCR req)
+        {
+            BaseResponse resp = new BaseResponse();
+            try
+            {
+                if (ValidateRequest.AddUser(req))
+                {
+                    UserCRUD userCRUD = new UserCRUD();
+                    if (!userCRUD.IsUserEmailExists(req.User.email_id))
+                    {
+                        req.User.last_update = DateTime.Now;
+                        if (userCRUD.AddUser(req.User) > 0)
+                        {
+                            resp.StatusCode = HttpStatusCode.OK;
+                            resp.StatusMessage = HttpStatusCode.OK.ToString();
+                        }
+                        else
+                        {
+                            resp.StatusCode = HttpStatusCode.Ambiguous;
+                            resp.StatusMessage = HttpStatusCode.Ambiguous.ToString();
+                        }
+                    }
+                    else
+                    {
+                        resp.StatusCode = HttpStatusCode.Conflict;
+                        resp.StatusMessage = HttpStatusCode.Conflict.ToString();
+                    }
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.StatusMessage = HttpStatusCode.BadRequest.ToString();
+                }
+            }
+            catch (Exception es)
+            {
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                resp.StatusMessage = HttpStatusCode.InternalServerError.ToString();
+            }
+            return resp;
+        }
+
+        [Route("UpdateUser")]
+        public BaseResponse UpdateUser(UserCR req)
+        {
+            BaseResponse resp = new BaseResponse();
+            try
+            {
+                if (ValidateRequest.UpdateUser(req))
+                {
+                    UserCRUD userCRUD = new UserCRUD();
+                    var email = JWTService.ValidateToken(req.Token);
+                    if (!string.IsNullOrEmpty(email) && email == req.User.email_id)
+                    {
+                        req.User.last_update = DateTime.Now;
+                        if (userCRUD.UpdateUser(req.User) > 0)
+                        {
+                            resp.StatusCode = HttpStatusCode.OK;
+                            resp.StatusMessage = HttpStatusCode.OK.ToString();
+                        }
+                        else
+                        {
+                            resp.StatusCode = HttpStatusCode.Ambiguous;
+                            resp.StatusMessage = HttpStatusCode.Ambiguous.ToString();
+                        }
+                    }
+                    else
+                    {
+                        resp.StatusCode = HttpStatusCode.Unauthorized;
+                        resp.StatusMessage = HttpStatusCode.Unauthorized.ToString();
+                    }
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.StatusMessage = HttpStatusCode.BadRequest.ToString();
+
+                }
+            }
+            catch (Exception es)
+            {
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                resp.StatusMessage = HttpStatusCode.InternalServerError.ToString();
+            }
+            return resp;
+        }
+
+
+        [Route("Profile")]
+        public UserResponse Profile(BaseResquest req)
+        {
+            UserResponse resp = new UserResponse();
+            try
+            {
+                if (ValidateRequest.Profile(req))
+                {
+                    UserCRUD userCRUD = new UserCRUD();
+                    var email = JWTService.ValidateToken(req.Token);
+                    if (!string.IsNullOrEmpty(email))
+                    {
+                        var user = userCRUD.GetUser(email);
+                        if (user != null)
+                        {
+                            resp.User = user;
+                            resp.User.password =null;
+                            resp.StatusCode = HttpStatusCode.OK;
+                            resp.StatusMessage = HttpStatusCode.OK.ToString();
+                        }
+                        else
+                        {
+                            resp.StatusCode = HttpStatusCode.NotFound;
+                            resp.StatusMessage = HttpStatusCode.NotFound.ToString();
+
+                        }
+                    }
+                    else
+                    {
+                        resp.StatusCode = HttpStatusCode.Unauthorized;
+                        resp.StatusMessage = HttpStatusCode.Unauthorized.ToString();
+                    }
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.StatusMessage = HttpStatusCode.BadRequest.ToString();
+
+                }
+            }
+            catch (Exception es)
+            {
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                resp.StatusMessage = HttpStatusCode.InternalServerError.ToString();
+            }
+            return resp;
+        }
+    }
+}
---
+++ UserManagement/UserManagement/BAL/DbOperation/UserCRUD.cs
@@ -0,0 +1,86 @@
+﻿using DAL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.DbOperation
+{
+    public class UserCRUD
+    {
+        Entities db;
+
+        public UserCRUD()
+        {
+            db = new Entities();
+            db.Database.Connection.Open();
+        }
+
+        ~UserCRUD()
+        {
+            db.Database.Connection.Close();
+        }
+        public user GetUser(string emailID)
+        {
+            var user = db.users.Where(x => x.email_id == emailID).FirstOrDefault();
+            return user;
+        }
+        public user GetUser(string emailAddress, string password)
+        {
+            var user = db.users.Where(x => x.email_id.ToLower().Equals(emailAddress.ToLower()) && x.password.ToLower().Equals(password.ToLower())).FirstOrDefault();
+            return user;
+        }
+        public bool IsUserValid(string emailAddress, string password)
+        {
+            var status = db.users.Any(x => x.email_id.ToLower().Equals(emailAddress.ToLower()) && x.password.ToLower().Equals(password.ToLower()));
+            return status;
+        }
+
+
+        public bool IsUserEmailExists(string emailAddress)
+        {
+            var status = db.users.Any(x => x.email_id.ToLower().Equals(emailAddress.ToLower()));
+            return status;
+        }
+        public bool IsUserActive(string emailAddress)
+        {
+            var status = db.users.Any(x => x.email_id.ToLower().Equals(emailAddress.ToLower()) && x.is_active == true);
+            return status;
+        }
+        public bool IsUserLocked(string emailAddress)
+        {
+            var status = db.users.Any(x => x.email_id.ToLower().Equals(emailAddress.ToLower()) && x.is_locked == true);
+            return status;
+        }
+
+        public int AddUser(user user)
+        {
+            user.is_active = true;
+            user.is_locked = false;
+            db.Entry(user).State = System.Data.Entity.EntityState.Added;
+            db.users.Add(user);
+            return db.SaveChanges();
+        }
+        public int UpdateUser(user user)
+        {
+            var tempUser = GetUser(user.email_id);
+
+            if (!string.IsNullOrEmpty(user.email_id))
+                tempUser.email_id = user.email_id;
+            if (!string.IsNullOrEmpty(user.first_name))
+                tempUser.first_name = user.first_name;
+            if (!string.IsNullOrEmpty(user.last_name))
+                tempUser.last_name = user.last_name;
+            if (!string.IsNullOrEmpty(user.password))
+                tempUser.password = user.password;
+            if (user.is_active != null)
+                tempUser.is_active = user.is_active;
+            if (user.is_locked != null)
+                tempUser.is_locked = user.is_locked;
+
+            return db.SaveChanges();
+        }
+
+    }
+}
